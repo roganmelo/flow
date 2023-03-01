e09bc6d212d20519e165cdf7511c860c7dd5e724
Rogan Melo <roganmelo@gmail.com>
2023-03-01 20:22:11 -0300

    first commit

---
+++ dts-bundle-generator.config.ts
@@ -0,0 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const packageJson = require('./package.json')
+
+const packageName = packageJson.name
+
+const config = {
+  entries: [
+    {
+      filePath: './src/index.ts',
+      outFile: `./dist/${packageName}.d.ts`,
+      noCheck: false
+    }
+  ]
+}
+
+module.exports = config
---
+++ vite.config.ts
@@ -0,0 +1,37 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+import packageJson from './package.json'
+
+const packageName = packageJson.name.replace('@', '').replace('/', '-')
+
+const getPackageNameCamelCase = () => {
+  try {
+    return packageName.replace(/-./g, char => char[1].toUpperCase())
+  } catch (err) {
+    throw new Error('Name property in package.json is missing.')
+  }
+}
+
+const fileName = {
+  es: `${packageName}.mjs`,
+  cjs: `${packageName}.cjs`,
+  iife: `${packageName}.iife.js`
+}
+
+const formats = Object.keys(fileName) as Array<keyof typeof fileName>
+
+module.exports = defineConfig({
+  base: './',
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: getPackageNameCamelCase(),
+      formats,
+      fileName: format => {
+        const extension = format as 'es' | 'cjs' | 'iife'
+
+        return fileName[extension]
+      }
+    }
+  }
+})
